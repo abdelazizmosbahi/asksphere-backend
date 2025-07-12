62db6996e186372bf21988f4260a188ed8765bc2
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-07-12 12:41:04 +0100

    Secure config.py by using only .env for SECRET_KEY and MONGO_URI

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-       SECRET_KEY = os.getenv('SECRET_KEY', '79da288f9993304133b20b3d2bffaa03c59055605dbf3ac9383e1fb0a151a245')
-       MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://mosbehiasiz:BSvss3YfLyb0ojMa@cluster0.ntfhykc.mongodb.net/asksphere?retryWrites=true&w=majority')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MONGO_URI = os.getenv('MONGO_URI')

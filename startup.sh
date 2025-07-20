7d2a7f2d40669c9cefa9c4bfc3ee9485669cca06
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-07-20 14:16:56 +0100

    Reduce Gunicorn workers to 1

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
+cd C:\NJH\iteam\pfa\asksphere\original_backend
+Set-Content -Path startup.sh -Value @'
 #!/bin/bash
 echo "Installing dependencies..."
 pip install -r requirements.txt > pip_install.log 2>&1
 echo "Starting Gunicorn..."
-gunicorn --workers=2 --bind=0.0.0.0:$PORT --log-level=debug --access-logfile=- --error-logfile=- app:app
+gunicorn --workers=1 --bind=0.0.0.0:$PORT --log-level=debug --access-logfile=- --error-logfile=- app:app
+'@
